1eb369e08a048690d4bba269b185a78db3d1ea12
Carmine Di Martino <cardimart@gmail.com>
2019-02-27 19:28:10 +0100

    Test gulp and pug and define default Gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,4 @@ gulp.task("pug", function() {
     .pipe(gulp.dest("build")); // tell gulp our output folder
 });
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
-
-exports.default = defaultTask;
+gulp.task("default", gulp.series("pug"));
