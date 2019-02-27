7f301ab8232052cb5c6843f530404dc999d0d373
Carmine Di Martino <cardimart@gmail.com>
2019-02-27 19:16:16 +0100

    Setting up gulp and pug.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+
+// run this task by typing in gulp pug in CLI
+gulp.task("pug", function() {
+  return gulp
+    .src("./*.pug")
+    .pipe(pug()) // pipe to pug plugin
+    .pipe(gulp.dest("build")); // tell gulp our output folder
+});
+
 function defaultTask(cb) {
   // place code for your default task here
   cb();
