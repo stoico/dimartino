27c893137bcfcfce576a0ac9f1a60f34fa045795
Carmine Di Martino <cardimart@gmail.com>
2019-02-27 23:28:23 +0100

    Generate index files from the pug templates/pages.

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,13 @@ const pug = require("gulp-pug");
 // run this task by typing in 'gulp pug' in CLI
 gulp.task("pug", function() {
   return gulp
-    .src("templates/*.pug")
-    .pipe(pug()) // pipe to pug plugin
+    .src("templates/pages/*.pug")
+    .pipe(
+      pug({
+        doctype: "html",
+        pretty: false
+      })
+    ) // pipe to pug plugin
     .pipe(gulp.dest("build")); // tell gulp our output folder
 });
 
